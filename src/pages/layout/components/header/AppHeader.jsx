108b50996cb5d46a99899fe2c17c15790ee8e4d4
Cristian Sosa <bossun032@gmail.com>
2023-11-05 02:45:55 -0300

    agregue el boton del usuario

--- src/pages/layout/components/header/AppHeader.jsx
+++ src/pages/layout/components/header/AppHeader.jsx
@@ -1,7 +1,9 @@
 import { React, useState } from 'react'
 import './AppHeader.css'
 import logo from './assets/logo.png'
+import usuario from './assets/usuario.png'
 import './AppHeader.css'
+
 
 const styles= {
   img: {
@@ -11,6 +13,13 @@ const styles= {
     fontSize: '30px',
     paddingTop: '28px',
     paddingLeft: '30px',
+  },
+  ingresar:{
+    width: '30px'
+  },
+  botonIngresar:{
+    borderRadius: '10px',
+    marginRight: '200px',
   }
 }
 
@@ -18,9 +27,11 @@ function AppHeader() {
 
   return (
     <div className='header'>
+            <button style={styles.botonIngresar}><img style={styles.ingresar} src={usuario} alt="" />ingresar</button>
             <img style={styles.img} src={logo} alt="logo" className="logoImg" />
             <br />
             <h1 style={styles.texto}>CHANGARINES - VERDULERIA - POLLERIA</h1>
+            
     </div>
   )
 }
